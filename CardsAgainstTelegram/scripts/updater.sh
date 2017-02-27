899c1af7296a6f83eccee92527f3bfde675dc17f
Amir <amir@aaomidi.com>
2017-02-26 23:01:25 -0500

    New pipelines

--- CardsAgainstTelegram/scripts/updater.sh
+++ CardsAgainstTelegram/scripts/updater.sh
@@ -2,6 +2,7 @@
 cd ~
 git clone git@bitbucket.org:jrh379-drexel/team03-drexel-16.git
 cd team03-drexel-16
+git pull
 cd CardsAgainstTelegram
 mvn clean package
 cp -f jar/CardsAgainstTelegram.jar ~/CardsAgainstTelegram.jar
